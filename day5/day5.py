f918fa78de53a0d8f467f94fc35ee4155718e872
Nick <nickhowlett13@gmail.com>
2021-12-30 21:42:53 +0800

    updating day5

--- day5/day5.py
+++ day5/day5.py
@@ -8,23 +8,35 @@ class ventLines:
         self.end = end
 
 
+class oceanFloor:
+    def __init__(self, sizeOfGrid):
+        self.grid = self.Makegrid(sizeOfGrid)
+
+    def Makegrid(self, size):
+        the_grid = []
+        for row in range(size):
+            the_row = []
+            for col in range(size):
+                the_row.append('.')
+            the_grid.append(the_row)
+
+        return the_grid
+
+    def print(self):
+        for row in self.grid:
+            for col in row:
+                print(col, end="")
+            print()
+
 def get_data(filePath):
     with open(filePath, 'r') as openFile:
         data = openFile.read()
 
     return data
 
-def grid(size):
-    the_grid = []
-    for row in range(size):
-        the_row = []
-        for col in range(size):
-            the_row.append('.')
-        the_grid.append(the_row)
 
-    return the_grid
 
-readData = get_data(r"C:\Users\nickk\Documents\Code\Python\AdventOfCode2021\day5Data.txt")
+readData = get_data(r"C:\Users\nickk\Documents\Code\Python\AdventOfCode2021\AdventOfCode2021\day5\day5Data.txt")
 readTest = [x.split(" -> ") for x in readData.split("\n")]
 lines = []
 
@@ -34,9 +46,7 @@ for corrod in readTest:
     x = ventLines(beg, end)
     lines.append(x)    
 
-small_grid = grid(10)
 
-small_grid[0][6] = 'X'
+x = oceanFloor(10)
 
-for t in small_grid:
-    print(t)
+x.print()
